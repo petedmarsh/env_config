6531280534f4a949b1638e7af9a3ce01544fb091
Peter Marsh <pete.d.marsh@gmail.com>
2021-12-26 00:20:40 +0000

    Move tests around so they test FromEnvVar generically
    bool type is used as it's the only one implemented so far. The tests
    for bool overlap with the internal Rust tests for FromStr but they're
    there to illustrate exactly how FromEnvVar works in the case of bools.

--- env_config/src/lib.rs
+++ env_config/src/lib.rs
@@ -48,30 +48,53 @@ mod tests {
     mod fromenvvar {
         use super::*;
 
+        #[test]
+        fn when_env_var_not_present() {
+            let unset: Option<String> = None;
+            temp_env::with_var(ENV_VAR, unset, || {
+                assert_eq!(<bool as FromEnvVar>::from_env_var(ENV_VAR).unwrap(), None);
+            });
+        }
+
+        #[test]
+        fn when_env_var_name_is_borrowed_string() {
+            let env_var_name_string = ENV_VAR.to_string();
+            temp_env::with_var(ENV_VAR, Some("false"), || {
+                assert_eq!(<bool as FromEnvVar>::from_env_var(&env_var_name_string).unwrap(), Some(false));
+            });
+        }
+
+        #[test]
+        fn when_env_var_is_valid_unicode_but_not_parsable() {
+            let invalid = "this is not a bool";
+            temp_env::with_var(ENV_VAR, Some(invalid), || {
+                let_assert!(Err(ConfigEnvError::NotParsable{ env_var_name, env_var_value }) = <bool as FromEnvVar>::from_env_var(ENV_VAR));
+                check!(env_var_name == ENV_VAR);
+                check!(env_var_value == invalid);
+            });
+        }
+
+        #[test]
+        fn when_env_var_is_not_valid_unicode() {
+            // Here, the values 0x66 and 0x6f correspond to 'f' and 'o'
+            // respectively. The value 0x80 is a lone continuation byte, invalid
+            // in a UTF-8 sequence.
+            let os_str = OsStr::from_bytes(&[0x66, 0x6f, 0x80, 0x6f]);
+            temp_env::with_var(ENV_VAR, Some(os_str.clone()), || {
+                let_assert!(Err(ConfigEnvError::NotUnicode{ env_var_name, env_var_raw_value }) = <bool as FromEnvVar>::from_env_var(ENV_VAR));
+                check!(env_var_name == ENV_VAR);
+                check!(env_var_raw_value == os_str);
+            });
+        }
+
         mod for_bool {
             use super::*;
             use temp_env;
 
             #[test]
-            fn when_env_var_not_present() {
-                let unset: Option<String> = None;
-                temp_env::with_var(ENV_VAR, unset, || {
-                    assert_eq!(<bool as FromEnvVar>::from_env_var(ENV_VAR).unwrap(), None);
-                });
-            }
-
-            #[test]
             fn when_env_var_is_false() {
                 temp_env::with_var(ENV_VAR, Some("false"), || {
                     assert_eq!(<bool as FromEnvVar>::from_env_var(ENV_VAR).unwrap(), Some(false));
-                });
-            }
-
-            #[test]
-            fn when_env_var_name_is_borrowed_string() {
-                let env_var_name_string = ENV_VAR.to_string();
-                temp_env::with_var(ENV_VAR, Some("false"), || {
-                    assert_eq!(<bool as FromEnvVar>::from_env_var(&env_var_name_string).unwrap(), Some(false));
                 });
             }
 
@@ -99,29 +122,6 @@ mod tests {
                     let_assert!(Err(ConfigEnvError::NotParsable{ env_var_name, env_var_value }) = <bool as FromEnvVar>::from_env_var(ENV_VAR));
                     check!(env_var_name == ENV_VAR);
                     check!(env_var_value == improperly_cased_false);
-                });
-            }
-
-            #[test]
-            fn when_env_var_is_valid_unicode_but_not_parsable() {
-                let invalid = "this is not a bool";
-                temp_env::with_var(ENV_VAR, Some(invalid), || {
-                    let_assert!(Err(ConfigEnvError::NotParsable{ env_var_name, env_var_value }) = <bool as FromEnvVar>::from_env_var(ENV_VAR));
-                    check!(env_var_name == ENV_VAR);
-                    check!(env_var_value == invalid);
-                });
-            }
-
-            #[test]
-            fn when_env_var_is_not_valid_unicode() {
-                // Here, the values 0x66 and 0x6f correspond to 'f' and 'o'
-                // respectively. The value 0x80 is a lone continuation byte, invalid
-                // in a UTF-8 sequence.
-                let os_str = OsStr::from_bytes(&[0x66, 0x6f, 0x80, 0x6f]);
-                temp_env::with_var(ENV_VAR, Some(os_str.clone()), || {
-                    let_assert!(Err(ConfigEnvError::NotUnicode{ env_var_name, env_var_raw_value }) = <bool as FromEnvVar>::from_env_var(ENV_VAR));
-                    check!(env_var_name == ENV_VAR);
-                    check!(env_var_raw_value == os_str);
                 });
             }
         }
